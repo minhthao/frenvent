26a3cd397233a893cddf9cdbf5d7d784ebe29698
minhthao <minhthaodangnguyen@gmail.com>
2014-06-10 00:55:21 -0700

    initial commit

---
+++ frenvent/frenvent/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  frenvent
+//
+//  Created by Minh Thao Nguyen on 6/10/14.
+//  Copyright (c) 2014 frenvent. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
