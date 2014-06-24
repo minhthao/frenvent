b5a29174a30dc5601543b3e70a1d0e3b0242b01f
minhthao <minh commit>
2014-06-23 23:43:35 -0700

    initial commit

---
+++ Frenvent/Frenvent/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Frenvent
+//
+//  Created by minh thao nguyen on 6/23/14.
+//  Copyright (c) 2014 Frenvent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
