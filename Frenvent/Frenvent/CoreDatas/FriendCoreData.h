cd869caaea583a5beb2259138552b35d4a88de87
minhthao <minh commit>
2014-06-27 00:44:14 -0700

    complete core data object management

---
+++ Frenvent/Frenvent/CoreDatas/FriendCoreData.h
@@ -0,0 +1,13 @@
+//
+//  FriendCoreData.h
+//  Frenvent
+//
+//  Created by minh thao nguyen on 6/26/14.
+//  Copyright (c) 2014 Frenvent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FriendCoreData : NSObject
+
+@end
---
+++ Frenvent/Frenvent/CoreDatas/FriendCoreData.m
@@ -0,0 +1,13 @@
+//
+//  FriendCoreData.m
+//  Frenvent
+//
+//  Created by minh thao nguyen on 6/26/14.
+//  Copyright (c) 2014 Frenvent. All rights reserved.
+//
+
+#import "FriendCoreData.h"
+
+@implementation FriendCoreData
+
+@end
