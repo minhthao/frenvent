31f3752a7ed6bf5c4adc81932f3d5290cade4638
minhthao <minh commit>
2014-07-19 02:57:47 -0700

    event detail view

---
+++ Frenvent/Frenvent/ViewControllers/EventViews/EventDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailViewController.h
+//  Frenvent
+//
+//  Created by minh thao nguyen on 7/19/14.
+//  Copyright (c) 2014 Frenvent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailViewController : UIViewController
+
+@end
